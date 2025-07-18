5fb8cb5aa024d9987d9b5890d0a7e000779995ee
mesh98a <167962988+mesh98a@users.noreply.github.com>
2025-07-18 11:04:09 +0200

    +

--- src/flags.js
+++ src/flags.js
@@ -7,12 +7,12 @@ const flagTextures = {};
 const countryCodes = [
   'AE','AF','AG','AL','AM','AO','AQ','AR','AT','AU','AZ','BA','BB','BD','BE','BF','BG','BH','BI','BJ','BN','BO','BR',
   'BS','BT','BW','BY','BZ','CA','CD','CF','CG','CH','CI','CL','CM','CN','CO','CR','CU','CV','CY','CZ','DE','DJ','DK',
-  'DM','DO','DZ','EC','EE','EG','EH','ER','ES','ET','FI','FJ','FK','FR','GA','GB','GD','GE','GH','GL','GM','GN','GQ',
+  'DM','DO','DZ','EC','EE','EG','EH','ER','ES','ET','FI','FJ','FK','FR','GA','GB','GE','GH','GL','GM','GN','GQ',
   'GR','GT','GW','GY','HN','HR','HT','HU','ID','IE','IL','IN','IQ','IR','IS','IT','JM','JO','JP','KE','KG','KH','KM',
-  'KP','KR','KW','KZ','LA','LB','LC','LI','LK','LR','LS','LT','LU','LV','LY','MA','MC','MD','ME','MG','MK','ML','MM',
-  'MN','MR','MT','MU','MV','MW','MX','MY','MZ','NA','NC','NE','NG','NI','NL','NO','NP','NZ','OM','PA','PE','PG','PH',
-  'PK','PL','PR','PS','PT','PY','QA','RO','RS','RU','RW','SA','SB','SD','SE','SG','SI','SK','SL','SM','SN','SO','SR',
-  'SS','SV','SY','SZ','TD','TG','TH','TJ','TL','TM','TN','TR','TT','TW','TZ','UA','UG','US','UY','UZ','VA','VE','VN',
+  'KP','KR','KW','KZ','LA','LB','LK','LR','LS','LT','LU','LV','LY','MA','MD','ME','MG','MK','ML','MM',
+  'MN','MR','MU','MW','MX','MY','MZ','NA','NC','NE','NG','NI','NL','NO','NP','NZ','OM','PA','PE','PG','PH',
+  'PK','PL','PR','PS','PT','PY','QA','RO','RS','RU','RW','SA','SB','SD','SE','SI','SK','SL','SN','SO','SR',
+  'SS','SV','SY','SZ','TD','TG','TH','TJ','TL','TM','TN','TR','TT','TW','TZ','UA','UG','US','UY','UZ','VE','VN',
   'VU','XK','YE','ZA','ZM','ZW'
 ];
 
